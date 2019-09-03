7a7bdf0bf819bd924728465059324147d65ebf6d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-09-03 17:55:04 +0200

    proper labeling of the figure

--- grass_scene/histograms.py
+++ grass_scene/histograms.py
@@ -1,6 +1,9 @@
+ print(entry_duration_list)
+ import matplotlib.pyplot as plt
+ plt.hist(entry_duration_list, bins = 20)
+ plt.xlabel('time (s)')
+ plt.ylabel('frequency')
+ plt.title('beacon stays')
+ plt.savefig('hist_%s ' % time_stamp)
+ plt.show()
 
-
-import matplotlib.pyplot as plt
-entry_duration_list = [33,4,23,45,6]
-plt.hist(entry_duration_list, bins = 20)
-plt.show()
--- grass_scene/renderer.py
+++ grass_scene/renderer.py
@@ -221,6 +221,9 @@ def main():
 
         #show histogram of beacon movement
         plt.hist(entry_duration_list, bins = 20)
+        plt.xlabel('time (s)')
+        plt.ylabel('frequency')
+        plt.title('beacon stays')
         plt.savefig('hist_%s ' % time_stamp)
         plt.show()
 
